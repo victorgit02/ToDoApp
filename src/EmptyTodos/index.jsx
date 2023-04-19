31e7151662efd0e1ed1fda10d607ea0d4e4794f9
Victor Areiza <victorareiza02@gmail.com>
2023-04-19 10:48:16 -0500

    Updates

--- src/EmptyTodos/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import "../EmptyTodos/EmptyTodos.css";
-
-function EmptyTodos () {
-    return <span className='EmptyContainer'>
-        <p className='CrearTodo'>!Crea tu Propio ToDo</p>
-        </span>}
-
-export {EmptyTodos};
